7030bb4189c894c6ff6c924148de831ab720a832
Monsteroooo <wmovchun2016@gmail.com>
2026-09-06 09:27:20 +0300

    Configure swagger to show xml comments

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using ABP_test.Data;
 using ABP_test.Services;
 using ABP_test.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title       = "Conference Room Booking API",
+        Version     = "v1",
+        Description = "API для управління бронюванням конференц-залів. " +
+                      "Дозволяє додавати зали, бронювати їх та переглядати звіти."
+    });
+
+    // Enable XML comments in Swagger UI
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<IRoomService, RoomService>();
